33440a00626e678ef66f2cb7d692a698083fd01b
Xiaofei Cao <xc9pd@mst.edu>
2016-02-26 15:15:34 -0600

    start working on mobil host and mobil support server

---
+++ TokenRing/src/MobilSupportServer.java
@@ -0,0 +1,71 @@
+import java.util.ArrayDeque;
+
+/**
+ * Created by xc9pd on 2/26/2016.
+ */
+public class MobilSupportServer {
+
+    private ArrayDeque request;
+    private ArrayDeque grantQueue;
+    private boolean waitingRingReturn;
+
+    public MobilSupportServer successor;
+
+    public MobilSupportServer(){
+
+        this.waitingRingReturn=false;
+        this.successor=null;
+    }
+
+    public void insertSuccessor(MobilSupportServer successor){
+        if(this.successor==null){
+            this.successor=successor;
+
+        }else{
+            successor.successor=this.successor;
+            this.successor=successor;
+        }
+    }
+
+    public boolean deleteSuccessor(){
+        if(this.successor==null){
+            return false;
+        }else{
+            this.successor=null;
+            return true;
+        }
+    }
+
+    public void executeToken(){
+
+    }
+
+    public void addRequest(Quest q){
+
+    }
+
+    public void putToGrantQueue(){
+
+    }
+
+    public void forwardToken(){
+
+    }
+
+    public void excuteQueue(){
+
+    }
+
+    public boolean newMobilHostArrive(){
+
+
+        return true;
+    }
+
+
+
+
+
+
+
+}
