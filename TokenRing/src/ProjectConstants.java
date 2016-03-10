af2044bd47475279ad4164bdece040cff18587a4
LoneWalker <azharul.parvez@gmail.com>
2016-03-10 12:56:36 -0600

    ControlUI and Constants added

---
+++ TokenRing/src/ProjectConstants.java
@@ -0,0 +1,20 @@
+import java.io.File;
+
+/**
+ * Created by azhar on 3/10/16.
+ */
+public class ProjectConstants {
+
+    public static String SCHEME_INFORM="Inform Scheme";
+    public static String SCHEME_PROXY="Proxy Scheme";
+    public static String SCHEME_REPLICATION="Replication Scheme";
+
+    public static String CURRENT_MODE;
+
+    //for node coloring
+    public static int NODE_COLOR_DEFAULT=0;
+    public static int NODE_COLOR_DELETED=1;
+    public static int NODE_COLOR_UPDATED=2;
+    public static int NODE_COLOR_CALLTRACE=3;
+
+}
